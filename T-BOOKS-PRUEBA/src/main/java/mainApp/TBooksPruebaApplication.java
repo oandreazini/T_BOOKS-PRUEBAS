70532d4068932f2a3f374e4897fb48147ea1f2de
oandreazini <oriol_as_92@hotmail.com>
2022-08-19 12:28:33 +0200

    src puch

---
+++ T-BOOKS-PRUEBA/src/main/java/mainApp/TBooksPruebaApplication.java
@@ -0,0 +1,13 @@
+package mainApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TBooksPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TBooksPruebaApplication.class, args);
+	}
+
+}
---
+++ T-BOOKS-PRUEBA/src/test/java/mainApp/TBooksPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package mainApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TBooksPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
