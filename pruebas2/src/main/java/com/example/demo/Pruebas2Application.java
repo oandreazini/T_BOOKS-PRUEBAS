5f6b8822375a9dd3b0aadf703e55d71afd1e96c2
Bárbara Broto <barbara.broto97@gmail.com>
2022-08-19 12:29:51 +0200

    asdfffff

---
+++ pruebas2/src/main/java/com/example/demo/Pruebas2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pruebas2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pruebas2Application.class, args);
+	}
+
+}
